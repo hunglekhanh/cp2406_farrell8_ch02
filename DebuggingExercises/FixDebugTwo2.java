ef94d19d0a1fab3400a3430c838995d834c54954
hunglekhanh <khanhhung.le@my.jcu.edu.au>
2017-07-21 15:17:05 +0800

    prac-02 solution

---
+++ DebuggingExercises/FixDebugTwo2.java
@@ -0,0 +1,16 @@
+public class DebugTwo2
+//  This application performs arithmetic with two integers
+{
+   public static void main(String args[])
+   {
+      int a, b;
+      a = 7;
+      b = 4;
+      System.out.print("The sum is ");
+      System.out.println(a+b);
+      System.out.print("The difference is ");
+      System.out.println(a-b);
+      System.out.print("The product is ");
+      System.out.println(a*b);
+   }
+}
